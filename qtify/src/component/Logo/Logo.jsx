644868409b81d9c867b2e54d3a338311e954e915
dream023se <dubeysanchika82@gmail.com>
2024-05-03 16:40:01 +0530

    modified

--- qtify/src/component/Logo/Logo.jsx
+++ qtify/src/component/Logo/Logo.jsx
@@ -5,7 +5,7 @@ export default function Logo()
 {
     return (
    <>
-   <img src={LogoImage} alt="Qtifylogo" height={34} width={67}/> 
+   <img src={LogoImage} alt="Qtifylogo" left={32} height={34} width={67}/> 
    </> 
     )
 }
