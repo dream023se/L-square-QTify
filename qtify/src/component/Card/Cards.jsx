c88ec67dc897ec42f99c9c8f3b087b2743a03b6b
dream023se <dubeysanchika82@gmail.com>
2024-05-04 20:10:59 +0530

    tooltip

--- qtify/src/component/Card/Cards.jsx
+++ qtify/src/component/Card/Cards.jsx
@@ -3,7 +3,7 @@ import Card from '@mui/material/Card';
 import CardContent from '@mui/material/CardContent';
 import CardMedia from '@mui/material/CardMedia';
 import Typography from '@mui/material/Typography';
-import { Box, CardActionArea } from '@mui/material';
+import { Box, CardActionArea, Tooltip } from '@mui/material';
 import styles from './Card.module.css'
 import Chip from '@mui/material/Chip';
 
@@ -11,6 +11,7 @@ const Cards = ({data,type}) => {
   const followText = data.follows+" Follows";
   const likeText = data.likes+" Likes"
   return (
+    <Tooltip title={type==='album'?(data.songs.length+" songs"):("")} placement={type==='album' ? ("top") : ("")} arrow>
     <Box className={styles.cardContainer}>
       <Card className={styles.customCard}>
       <CardActionArea>
@@ -32,6 +33,7 @@ const Cards = ({data,type}) => {
    </Typography>
    
    </Box>
+   </Tooltip>
   )
 }
 
